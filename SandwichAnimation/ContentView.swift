20aca0e5c1dce17e7e797f40b8a6425ce149aa43
Stecalovici Iuliana <iulianastecalovici@coherentsolutions.com>
2024-07-26 17:35:18 +0300

    Initial Commit

---
+++ SandwichAnimation/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SandwichAnimation
+//
+//  Created by Iuliana Stecalovici on 7/26/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SandwichAnimation/SandwichAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichAnimationApp.swift
+//  SandwichAnimation
+//
+//  Created by Iuliana Stecalovici on 7/26/24.
+//
+
+import SwiftUI
+
+@main
+struct SandwichAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
